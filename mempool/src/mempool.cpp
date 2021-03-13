6273c369adb00a1662f3a25772306ea3c222f60e
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2021-03-13 14:50:29 +0100

    update mempool example

--- mempool/src/mempool.cpp
+++ mempool/src/mempool.cpp
@@ -1,55 +1,58 @@
 #include <miniThread.hpp>
 
 using namespace mn;
-using namespace mn::memory;
 
 #define TEST_ELEMENTS 10
 
-struct mempool_test_buffer {
-    int used;
+
+
+
+struct mempool_tester {
+    using mempool_t = mn::memory::basic_mempool_t<int, TEST_ELEMENTS>;
+
+    mempool_tester() {
+        __mempool.create(portMAX_DELAY);
+    }
+    void test(){
+        while(true) {
+            test_allocate();
+            test_free();
+
+            basic_task::sleep(1);
+        }
+    }
+    void test_allocate( ) {
+        std::string text = "";
+        std::cout << "allocated test" << std::endl << std::endl;
+
+        for(int i= 0; i < TEST_ELEMENTS + 1; i++) {
+            test_array[i] = (int*)__mempool.allocate(true, 200);
+
+            text = ( (test_array[i] == NULL) ? " empty" : " ok" );
+            std::cout << i << text << std::endl;
+        }
+    }
+    void test_free() {
+        std::string text = "";
+        std::cout << "Free test " << std::endl << std::endl;
+        
+        for(int i= 0; i < TEST_ELEMENTS + 1; i++) {
+            text = __mempool.free(test_array[i]) ? " free" : " not free";
+            std::cout << i << text << std::endl;
+        }
+    }
+
+    mempool_t __mempool;
+    int* test_array[TEST_ELEMENTS + 1];
 };
 
-int* test_array[TEST_ELEMENTS + 1];
-
-void mempool_test_allocate(basic_mempool_t *mempool ) {
-    std::string text = "";
-    std::cout << "allocated test" << std::endl << std::endl;
-
-    for(int i= 0; i < TEST_ELEMENTS + 1; i++) {
-        test_array[i] = (int*)mempool->allocate(200);
-
-        text = ( (test_array[i] == NULL) ? " empty" : " ok" );
-        std::cout << i << text << std::endl;
-    }
-}
-void mempool_test_free(basic_mempool_t *mempool) {
-    std::string text = "";
-    std::cout << "Free test " << std::endl << std::endl;
-
-    for(int i= 0; i < TEST_ELEMENTS + 1; i++) {
-        text = mempool->free(test_array[i], 200) ? " free" : " not free";
-        std::cout << i << text << std::endl;
-    }
-}
 
 extern "C" void app_main() {
     std::cout << "Thank you for use miniThread v. " << mn::version::instance().to_string() << std::endl;
-    std::cout << "simple mempool test" << std::endl;
+    std::cout << "simple __mempool test" << std::endl;
 
-    basic_mempool_t *mempool = new basic_mempool_t(sizeof(mempool_test_buffer), TEST_ELEMENTS);
-    err_cheak(test->create(portMAX_DELAY), NO_ERROR);
-
-    while(true) {
-        mempool_test_allocate(mempool):
-        mempool_test_free(mempool);
-
-        basic_task::sleep(1);
-    }
+    mempool_tester tester;
+    tester.test();
+    
 }
 
-void err_cheak(int error, int OK) {
-    switch(error) {
-        case OK: return;
-    };
-    panic();
-}
